b37a3f8ed8bdf106128c76e1fc309604c860a26e
Scot <bscot.hagen@gmail.com>
2021-08-05 16:53:42 -0700

    added new field and validations/data annotations

--- RazorPagesMovie23/Migrations/20210804185832_Rating.Designer.cs
+++ RazorPagesMovie23/Migrations/20210804185832_Rating.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMovie23.Data;
 
 namespace RazorPagesMovie23.Migrations
 {
     [DbContext(typeof(RazorPagesMovie23Context))]
-    partial class RazorPagesMovie23ContextModelSnapshot : ModelSnapshot
+    [Migration("20210804185832_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- RazorPagesMovie23/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie23/Pages/Movies/Index.cshtml.cs
@@ -29,15 +29,19 @@ namespace RazorPagesMovie23.Pages.Movies
 
         public async Task OnGetAsync()
         {
+            // Use LINQ to get list of genres.
             IQueryable<string> genreQuery = from m in _context.Movie
                                             orderby m.Genre
                                             select m.Genre;
+
             var movies = from m in _context.Movie
                          select m;
+
             if (!string.IsNullOrEmpty(SearchString))
             {
                 movies = movies.Where(s => s.Title.Contains(SearchString));
             }
+
             if (!string.IsNullOrEmpty(MovieGenre))
             {
                 movies = movies.Where(x => x.Genre == MovieGenre);
